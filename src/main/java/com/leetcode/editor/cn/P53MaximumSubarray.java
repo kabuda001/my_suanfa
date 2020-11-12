d5c18dd431627af9da7cb29f8c25c58b31567972
zhaoc <zhaoc@133.cn>
2020-11-12 15:20:01 +0800

    53:最大子序和

--- src/main/java/com/leetcode/editor/cn/P53MaximumSubarray.java
+++ src/main/java/com/leetcode/editor/cn/P53MaximumSubarray.java
@@ -21,15 +21,13 @@ public class P53MaximumSubarray {
             if (nums.length == 1) {
                 return nums[0];
             }
+            // 动态规划,dp[i]表示i结尾的子序最大和
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
             int res = nums[0];
-            int sum = 0;
-            for (int num : nums) {
-                if (sum > 0) {
-                    sum += num;
-                } else {
-                    sum = num;
-                }
-                res = Math.max(res, sum);
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+                res = Math.max(res, dp[i]);
             }
             return res;
         }
