1b54194e17c60e273cf9f4c47d11bab3722090e6
zhaoc <zhaoc@133.cn>
2020-11-13 16:18:45 +0800

    翻转链表

---
+++ src/main/java/com/leetcode/editor/cn/FanZhuanLianBiaoLcof.java
@@ -0,0 +1,60 @@
+package com.leetcode.editor.cn;
+//剑指 Offer 24:反转链表
+//定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 5000 
+//
+// 
+//
+// 注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/ 
+// Related Topics 链表 
+// 👍 130 👎 0
+
+import com.leetcode.base.ListNode;
+
+public class FanZhuanLianBiaoLcof {
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode cur = head;
+            ListNode pre = null;
+            while (cur != null) {
+                ListNode tmp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = tmp;
+            }
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new FanZhuanLianBiaoLcof().new Solution();
+    }
+}
