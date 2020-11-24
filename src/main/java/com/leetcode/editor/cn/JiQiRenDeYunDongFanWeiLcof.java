521a66e06e54d61341b94cf8cd36dd14e2050177
zhaoc <zhaoc@133.cn>
2020-11-24 18:10:32 +0800

    剑指 Offer 13:机器人的运动范围

--- src/main/java/com/leetcode/editor/cn/JiQiRenDeYunDongFanWeiLcof.java
+++ src/main/java/com/leetcode/editor/cn/JiQiRenDeYunDongFanWeiLcof.java
@@ -30,8 +30,17 @@ public class JiQiRenDeYunDongFanWeiLcof {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int movingCount(int m, int n, int k) {
-            // TODO: 2020/11/17 暂时
-            return 0;
+            boolean[][] visited = new boolean[m][n];
+            return this.dfs(0, 0, m, n, k, visited);
+        }
+
+        private int dfs(int i, int j, int m, int n, int k, boolean visited[][]) {
+            if (i < 0 || i >= m || j < 0 || j >= n || (i/10 + i%10 + j/10 + j%10) > k || visited[i][j]) {
+                return 0;
+            }
+            visited[i][j] = true;
+            return dfs(i + 1, j, m, n, k, visited) + dfs(i - 1, j, m, n, k, visited) +
+                    dfs(i, j + 1, m, n, k, visited) + dfs(i, j - 1, m, n, k, visited) + 1;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
