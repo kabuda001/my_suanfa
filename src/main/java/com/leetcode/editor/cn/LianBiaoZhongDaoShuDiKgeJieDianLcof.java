d1300dc742c8a30568d2b2522d400bb8d27ac1be
zhaoc <zhaoc@133.cn>
2020-11-25 18:58:02 +0800

    剑指 Offer 22:链表中倒数第k个节点

---
+++ src/main/java/com/leetcode/editor/cn/LianBiaoZhongDaoShuDiKgeJieDianLcof.java
@@ -0,0 +1,54 @@
+package com.leetcode.editor.cn;
+//剑指 Offer 22:链表中倒数第k个节点
+//输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。例如，一个链表有6个节点，从头节点开始，
+//它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。 
+//
+// 
+//
+// 示例： 
+//
+// 给定一个链表: 1->2->3->4->5, 和 k = 2.
+//
+//返回链表 4->5. 
+// Related Topics 链表 双指针 
+// 👍 109 👎 0
+
+import com.leetcode.base.ListNode;
+
+public class LianBiaoZhongDaoShuDiKgeJieDianLcof {
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode getKthFromEnd(ListNode head, int k) {
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode fast = dummy;
+            for (int i = 0; i < k; i++) {
+                fast = fast.next;
+                if (fast == null) {
+                    return head;
+                }
+            }
+            while (fast != null) {
+                fast = fast.next;
+                dummy = dummy.next;
+            }
+            return dummy;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new LianBiaoZhongDaoShuDiKgeJieDianLcof().new Solution();
+    }
+}
