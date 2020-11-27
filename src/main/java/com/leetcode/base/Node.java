bf808fac4545781f2ccd8f303d9fe6f288b359aa
zhaoc <zhaoc@133.cn>
2020-11-27 16:40:12 +0800

    剑指 Offer 35:复杂链表的复制

---
+++ src/main/java/com/leetcode/base/Node.java
@@ -0,0 +1,19 @@
+package com.leetcode.base;
+
+/**
+ * ListNode
+ *
+ * @author zhaoc
+ * @date 13 9:54 上午
+ */
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
